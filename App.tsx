243c7c7becc238b169bfb60006687103429790fb
Elias Gustafsson <elias_gustafsson@outlook.com>
2025-06-09 10:49:51 +0200

    test

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Game Journal</Text>
+      <Text>Hello?</Text>
       <StatusBar style="auto" />
     </View>
   );
