ac446a64a42b60059cc482ff02b184d012610555
Elias Gustafsson <elias_gustafsson@outlook.com>
2025-06-13 13:29:39 +0200

    statisticsPage

---
+++ components/xpBar/xpBar.tsx
@@ -0,0 +1,54 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { useGames } from "../../context/gameContext";
+import { theme } from "../../app/theme";
+
+export default function XPBar() {
+  const { xp } = useGames();
+  const XP_PER_LEVEL = 1000;
+  const currentLevel = Math.floor(xp / XP_PER_LEVEL) + 1;
+  const currentXp = xp % XP_PER_LEVEL;
+  const xpProgress = currentXp / XP_PER_LEVEL;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.levelText}>Level {currentLevel}</Text>
+      <View style={styles.barBackground}>
+        <View style={[styles.barFill, { width: `${xpProgress * 100}%` }]} />
+      </View>
+      <Text style={styles.xpText}>
+        {currentXp} / {XP_PER_LEVEL} XP
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "90%",
+    marginVertical: 20,
+    alignItems: "center",
+  },
+  levelText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: theme.color.textPrimary,
+    marginBottom: 5,
+  },
+  barBackground: {
+    width: "100%",
+    height: 20,
+    backgroundColor: theme.color.textPrimary,
+    borderRadius: 5,
+    overflow: "hidden",
+  },
+  barFill: {
+    height: "100%",
+    backgroundColor: theme.color.purpleColor,
+  },
+  xpText: {
+    marginTop: 5,
+    color: theme.color.textSecondary,
+  },
+});
--- context/gameContext.tsx
+++ context/gameContext.tsx
@@ -4,6 +4,7 @@ import { Game } from "../types/types";
 type GameContextType = {
   games: Game[];
   addGame: (game: Game) => void;
+  xp: number;
 };
 const GameContext = createContext<GameContextType | undefined>(undefined);
 export const useGames = () => {
@@ -14,14 +15,20 @@ export const useGames = () => {
   return context;
 };
 export const GameProvider = ({ children }: { children: ReactNode }) => {
+  const [xp, setXp] = useState(0);
   const [games, setGames] = useState<Game[]>([]);
 
   const addGame = (game: Game) => {
     setGames((prevGames) => [...prevGames, game]);
+
+    let newXp = 0;
+    if (game.status === "completed") newXp = 100;
+    if (game.status === "platinum") newXp = 500;
+    setXp((prevXp) => prevXp + newXp);
   };
 
   return (
-    <GameContext.Provider value={{ games, addGame }}>
+    <GameContext.Provider value={{ games, addGame, xp }}>
       {children}
     </GameContext.Provider>
   );
